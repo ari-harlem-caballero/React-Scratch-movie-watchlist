bc4fde2864f5cd493eb12b051261f61e704c1bf3
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-17 18:00:57 -0800

    added plan to tvshow/showlist

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default function TVShow() {
+  // props: tvshow
+  // *handleClick*
   return (
+    //tvshow.name, overview, poster_path *haveWatched*
     <div>TVShow</div>
   );
 }
--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default function TVShowList() {
+  // ??when to render <Movie> vs <WatchListItem> (URL)
+  // props: tvshows
   return (
+    //map tvshows(key=tvshow.title, tvshow=tvshow)
     <div>TVShowList</div>
   );
 }
