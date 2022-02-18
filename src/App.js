874663f4f267cc62056af97614d4fd44939dd5d6
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-17 16:42:37 -0800

    added plan for app/auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  NavLink
+} from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  // state: currentUser(localStorage.getItem('supabase.auth.token'))
+  
   return (
+    // router over all
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        {/* if user&&, render logout, Nav(searchpage, watchlist) */}
       </header>
+      {/* switch/route: exact paths if no!user (/=search|Auth, /search=/|search, /watchlist=/|watchlist */}
     </div>
   );
 }
--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function AuthPage() {
+  // maintain state: email, pass // props: setCurrentUser
+  // handle: signIn, signUp (fetch, set, clear)
+
   return (
+    // form, label (email, pass), button
     <div>AuthPage</div>
   );
 }
