d5dc502aa23dc85f341d6bb1d74c01a5a78e6fc2
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-18 09:48:34 -0800

    created TVShow componenet

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export default function TVShow() {
+export default function TVShow({ tvshow }) {
   // props: tvshow
   // *handleClick*
+
   return (
     //tvshow.name, overview, poster_path *haveWatched*
-    <div>TVShow</div>
+    <div>
+      <h2>{tvshow.name}</h2>
+      <p>{tvshow.overview}</p>
+      <img src={tvshow.poster_path ? `https://image.tmdb.org/t/p/original${tvshow.poster_path}` : `./film.png`} alt="poster image of tv-show"/>
+    </div>
   );
 }
