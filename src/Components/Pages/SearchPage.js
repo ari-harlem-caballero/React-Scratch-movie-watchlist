ab857c0246ce0bd25512ac62540de29cce4d2a50
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-18 09:34:04 -0800

    created searchPage form

--- src/Components/Pages/SearchPage.js
+++ src/Components/Pages/SearchPage.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { searchTVShows } from '../../services/fetch-utils';
 
 export default function SearchPage() {
 // state: query, results, watchlist
-// effect: refresh
-// handleSearch (fetch searchShow(query), set:watch)
+  const [searchQuery, setSearchQuery] = useState('');
+  const [results, setResults] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  // handleSearch (fetch searchShow(query), set:watch)
+  async function handleSearch(e) {
+    e.preventDefault();
+
+    const tvshow = await searchTVShows(searchQuery);
+
+    setWatchlist(tvshow);
+  }
+
+// *effect: refresh*
+
 // *refresh (fetch Watchlist, set:watch)*
 // *onWatchlist(api_id): match weirdness*
 
   return (
     // form, input(change/value=query), button
     // Results: <List w/ results, *refresh*>
-    <div>SearchPage</div>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input 
+          onChange={e => setSearchQuery(e.target.value)}
+          value={searchQuery}/>
+        <button>Search Shows</button>
+      </form>
+      <section>
+        Results:
+      </section>
+    </div>
   );
 }
