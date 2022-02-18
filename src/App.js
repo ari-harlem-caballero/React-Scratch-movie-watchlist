38b867592f6e7e24d56335abaf5197d877a80513
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-17 17:00:11 -0800

    added App header

--- src/App.js
+++ src/App.js
@@ -6,19 +6,32 @@ import {
   NavLink
 } from 'react-router-dom';
 import { useState } from 'react';
+import { logout } from './services/fetch-utils';
 import './App.css';
 
 function App() {
   // state: currentUser(localStorage.getItem('supabase.auth.token'))
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('supabase.auth.token'));
   
+
   return (
     // router over all
-    <div className="App">
-      <header className="header">
-        {/* if user&&, render logout, Nav(searchpage, watchlist) */}
-      </header>
-      {/* switch/route: exact paths if no!user (/=search|Auth, /search=/|search, /watchlist=/|watchlist */}
-    </div>
+    <Router>
+      <div className="App">
+        <header className="header">
+          {/* if user&&, render logout, Nav(searchpage, watchlist) */}
+          {
+            currentUser &&
+            <>
+              <NavLink to='/search'>Search</NavLink>
+              <NavLink to='/watchlist'>Watchlist</NavLink>
+              <button onClick={logout}>Logout</button>
+            </>
+          }
+        </header>
+        {/* switch/route: exact paths if no!user (/=search|Auth, /search=/|search, /watchlist=/|watchlist */}
+      </div>
+    </Router>
   );
 }
 
