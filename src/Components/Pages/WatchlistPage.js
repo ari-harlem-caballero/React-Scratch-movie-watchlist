48d8484de0a2f6a417ced9d2b03648fadba0dc94
ari-harlem-caballero <ari.bash.harlem@gmail.com>
2022-02-18 16:14:53 -0800

    added WatchlistItem to tvshowlist

--- src/Components/Pages/WatchlistPage.js
+++ src/Components/Pages/WatchlistPage.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
+import TVShowList from '../TVShowList';
 
 export default function WatchlistPage() {
-  // state: movies
+  // state: tvshows
+  const [tvshows, setTvshows] = useState([]);
   // *refreshWatchList*: 
   return (
-    <div>WatchlistPage
+    <div>
       {/* tvshowList: tvshows */}
+      <h1>My Show Watchlist:</h1>
+      <TVShowList tvshows={tvshows} />
     </div>
   );
 }
--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
 import TVShow from './TVShow';
+import WatchlistItem from './WatchlistItem';
 
 export default function TVShowList({ tvshows }) {
   // ??when to render <Movie> vs <WatchListItem> (URL)
   // props: tvshows
+  const location = useLocation();
+  
   return (
     //map tvshows(key=tvshow.title, tvshow=tvshow)
     <div className='tvshow-list'>
       {
-        tvshows.map((tvshow, i) => 
-          <TVShow key={tvshow.title + i} 
+        tvshows.map((tvshow, i) => location.pathname.includes('search')
+          ? <TVShow 
+            key={tvshow.title + i} 
+            tvshow={tvshow} />
+          : <WatchlistItem 
+            key={tvshow.title + i}
             tvshow={tvshow} />)
       }
     </div>
